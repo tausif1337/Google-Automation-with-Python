1a729901abe8dc7e53ac4c84818a8fb2bfe4c752
Haosam <36357075+Haosam@users.noreply.github.com>
2020-06-02 18:21:28 +0800

    Update Network.py

--- Using Python to Interact with the Operating System/Week 1 - Graded Assignment/Network.py
+++ Using Python to Interact with the Operating System/Week 1 - Graded Assignment/Network.py
@@ -17,3 +17,7 @@ def check_connectivity():
                 return True
         else:
                 return False
+
+
+# Follow the instructions
+# request.status_code: According to documents, it is an object class called by request. It will return a status code like 200 or 404
